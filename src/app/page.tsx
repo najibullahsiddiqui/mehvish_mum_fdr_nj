7ab3d98be620d4698875861150cc98aae089a097
najibullahsiddiqui <30460848+najibullahsiddiqui@users.noreply.github.com>
2026-08-30 20:45:01 +0530

    Use focused cockpit as default app shell

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import { CreatorDashboard } from "@/components/CreatorDashboard";
+import { CreatorCockpit } from "@/components/CreatorCockpit";
 import { getDashboardData } from "@/lib/dashboard-data";
 
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const dashboard = await getDashboardData();
-
-  return <CreatorDashboard initialData={dashboard} />;
+  return <CreatorCockpit initialData={dashboard} />;
 }
